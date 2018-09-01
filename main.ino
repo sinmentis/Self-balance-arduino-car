acfbd5d1aec764ec840069a3d6f5ef07f53793b6
Shun <20508894+sinmentis@users.noreply.github.com>
2018-09-01 15:31:38 +1200

    First upload

---
+++ main.ino
@@ -0,0 +1,195 @@
+#include "SparkFunMPU9250-DMP.h"
+
+MPU9250_DMP imu;
+// Motor 1
+int dir1PinA = 2;
+int dir2PinA = 3;
+int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
+
+// Motor 2
+int dir1PinB = 4;
+int dir2PinB = 5;
+int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
+
+void setup() {  // Setup runs once per reset
+  Serial.begin(9600);
+  if (imu.begin() != INV_SUCCESS)
+  {
+    while (1)
+    {
+      Serial.println("Unable to communicate with MPU-9250");
+      Serial.println("Check connections, and try again.");
+      Serial.println();
+      delay(5000);
+    }
+  }
+
+  // Use setSensors to turn on or off MPU-9250 sensors.
+  // Any of the following defines can be combined:
+  // INV_XYZ_GYRO, INV_XYZ_ACCEL, INV_XYZ_COMPASS,
+  // INV_X_GYRO, INV_Y_GYRO, or INV_Z_GYRO
+  // Enable all sensors:
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+  // Use setGyroFSR() and setAccelFSR() to configure the
+  // gyroscope and accelerometer full scale ranges.
+  // Gyro options are +/- 250, 500, 1000, or 2000 dps
+  imu.setGyroFSR(2000); // Set gyro to 2000 dps
+  // Accel options are +/- 2, 4, 8, or 16 g
+  imu.setAccelFSR(2); // Set accel to +/-2g
+  // Note: the MPU-9250's magnetometer FSR is set at 
+  // +/- 4912 uT (micro-tesla's)
+
+  // setLPF() can be used to set the digital low-pass filter
+  // of the accelerometer and gyroscope.
+  // Can be any of the following: 188, 98, 42, 20, 10, 5
+  // (values are in Hz).
+  imu.setLPF(5); // Set LPF corner frequency to 5Hz
+
+  // The sample rate of the accel/gyro can be set using
+  // setSampleRate. Acceptable values range from 4Hz to 1kHz
+  imu.setSampleRate(10); // Set sample rate to 10Hz
+
+  // Likewise, the compass (magnetometer) sample rate can be
+  // set using the setCompassSampleRate() function.
+  // This value can range between: 1-100Hz
+  imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+  
+  // motor part below------------------------------
+  // initialize serial communication @ 9600 baud:
+  //Serial.begin(9600);
+
+  //Define L298N Dual H-Bridge Motor Controller Pins
+
+  pinMode(dir1PinA, OUTPUT);
+  pinMode(dir2PinA, OUTPUT);
+  pinMode(speedPinA, OUTPUT);
+  pinMode(dir1PinB, OUTPUT);
+  pinMode(dir2PinB, OUTPUT);
+  pinMode(speedPinB, OUTPUT);
+
+}
+
+void loop() {
+
+  // dataReady() checks to see if new accel/gyro data
+  // is available. It will return a boolean true or false
+  // (New magnetometer data cannot be checked, as the library
+  //  runs that sensor in single-conversion mode.)
+  if ( imu.dataReady() )
+  {
+    // Call update() to update the imu objects sensor data.
+    // You can specify which sensors to update by combining
+    // UPDATE_ACCEL, UPDATE_GYRO, UPDATE_COMPASS, and/or
+    // UPDATE_TEMPERATURE.
+    // (The update function defaults to accel, gyro, compass,
+    //  so you don't have to specify these values.)
+    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+    printIMUData();
+  }
+
+  // Initialize the Serial interface:
+
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    int speed; // Local variable
+
+    switch (inByte) {
+
+      //______________Motor 1______________
+
+      case '1': // Motor 1 Forward
+        analogWrite(speedPinA, 255);//Sets speed variable via PWM
+        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir2PinA, HIGH);
+        Serial.println("Motor 1  Forward"); // Prints out “Motor 1 Forward” on the serial monitor
+        Serial.println("   "); // Creates a blank line printed on the serial monitor
+
+        analogWrite(speedPinB, 255);
+        digitalWrite(dir1PinB, LOW);
+        digitalWrite(dir2PinB, HIGH);
+        Serial.println("Motor 2 Forward");
+        Serial.println("   ");
+        break;
+
+      case '2': // Motor 1 Stop (Freespin)
+        analogWrite(speedPinA, 0);
+        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir2PinA, HIGH);
+        Serial.println("Motor 1 Stop");
+        Serial.println("   ");
+        break;
+
+      case '3': // Motor 1 Reverse
+        analogWrite(speedPinA, 255);
+        digitalWrite(dir1PinA, HIGH);
+        digitalWrite(dir2PinA, LOW);
+        Serial.println("Motor 1 Reverse");
+        Serial.println("   ");
+        break;
+
+      //______________Motor 2______________
+
+      case '4': // Motor 2 Forward
+        analogWrite(speedPinB, 255);
+        digitalWrite(dir1PinB, LOW);
+        digitalWrite(dir2PinB, HIGH);
+        Serial.println("Motor 2 Forward");
+        Serial.println("   ");
+        break;
+
+      case '5': // Motor 1 Stop (Freespin)
+        analogWrite(speedPinB, 0);
+        digitalWrite(dir1PinB, LOW);
+        digitalWrite(dir2PinB, HIGH);
+        Serial.println("Motor 2 Stop");
+        Serial.println("   ");
+        break;
+
+      case '6': // Motor 2 Reverse
+        analogWrite(speedPinB, 255);
+        digitalWrite(dir1PinB, HIGH);
+        digitalWrite(dir2PinB, LOW);
+        Serial.println("Motor 2 Reverse");
+        Serial.println("   ");
+        break;
+
+      default:
+        // turn all the connections off if an unmapped key is pressed:
+        for (int thisPin = 2; thisPin < 11; thisPin++) {
+          digitalWrite(thisPin, LOW);
+        }
+    }
+  }
+}
+
+void printIMUData(void)
+{  
+  // After calling update() the ax, ay, az, gx, gy, gz, mx,
+  // my, mz, time, and/or temerature class variables are all
+  // updated. Access them by placing the object. in front:
+
+  // Use the calcAccel, calcGyro, and calcMag functions to
+  // convert the raw sensor readings (signed 16-bit values)
+  // to their respective units.
+  float accelX = imu.calcAccel(imu.ax);
+  float accelY = imu.calcAccel(imu.ay);
+  float accelZ = imu.calcAccel(imu.az);
+  float gyroX = imu.calcGyro(imu.gx);
+  float gyroY = imu.calcGyro(imu.gy);
+  float gyroZ = imu.calcGyro(imu.gz);
+  float magX = imu.calcMag(imu.mx);
+  float magY = imu.calcMag(imu.my);
+  float magZ = imu.calcMag(imu.mz);
+  
+  Serial.println("Accel: " + String(accelX) + ", " +
+              String(accelY) + ", " + String(accelZ) + " g");
+  Serial.println("Gyro: " + String(gyroX) + ", " +
+              String(gyroY) + ", " + String(gyroZ) + " dps");
+  Serial.println("Mag: " + String(magX) + ", " +
+              String(magY) + ", " + String(magZ) + " uT");
+  Serial.println("Time: " + String(imu.time) + " ms");
+  Serial.println();
+  delay(1000);
+}
+
