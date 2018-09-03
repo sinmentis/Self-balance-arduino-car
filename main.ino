9aa52a2b06ef97c7d27d29d07e554f11640036d0
Shun <20508894+sinmentis@users.noreply.github.com>
2018-09-03 14:52:17 +1200

    Adding wireless remote part

--- main.ino
+++ main.ino
@@ -1,27 +1,37 @@
 #include "SparkFunMPU9250-DMP.h"
 
-
-
 /*======================Global variable======================*/
+
+// MPU9250
 MPU9250_DMP imu;
 float accelX, accelY, accelZ;
 float gyroX, gyroY, gyroZ;
+float acc_angle_x, acc_angle_y;
+float gyr_angle_x, gyr_angle_y;
+float rad_to_reg = 180/3.141592654;
 
-// Motor 1
-int dir1PinA = 2;
-int dir2PinA = 3;
-int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
+// Motor
+int dir1_L_PIN = 2;
+int dir2_L_PIN = 3;
+int speed_L_PIN = 9;
+int dir1_R_PIN = 4;
+int dir2_R_PIN = 5;
+int speed_L_PIN = 10;
 
-// Motor 2
-int dir1PinB = 4;
-int dir2PinB = 5;
-int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
+// PID
+float kp = 4;
+float ki = 0.005;
+float kd = 2;
+float desired_angle = 0;
 
-
-
-
-
+// Receiver
+enum re_command {forward=1, backward=2, left=3, right=4, stay=0};
+int re_1 = 11;
+int re_2 = 8;
+int re_3 = 7;
+int re_4 = 6;
 /*======================support function======================*/
+
 void UpdateIMUData(void)
 {  
   accelX = imu.calcAccel(imu.ax);
@@ -30,31 +40,39 @@ void UpdateIMUData(void)
   gyroX = imu.calcGyro(imu.gx);
   gyroY = imu.calcGyro(imu.gy);
   gyroZ = imu.calcGyro(imu.gz);
+  acc_angle_x = atan(accelY/sqrt(pow(accelX,2)+pow(accelZ,2)))*rad_to_reg;
+  acc_angle_y = atan(-1*accelX/sqrt(pow(accelY,2)+pow(accelZ,2)))*rad_to_reg;
 }
 void printIMUData(void)
 {  
-  // After calling update() the ax, ay, az, gx, gy, gz, mx,
-  // my, mz, time, and/or temerature class variables are all
-  // updated. Access them by placing the object. in front:
-
-  // Use the calcAccel, calcGyro, and calcMag functions to
-  // convert the raw sensor readings (signed 16-bit values)
-  // to their respective units.
-  accelX = imu.calcAccel(imu.ax);
-  accelY = imu.calcAccel(imu.ay);
-  accelZ = imu.calcAccel(imu.az);
-  gyroX = imu.calcGyro(imu.gx);
-  gyroY = imu.calcGyro(imu.gy);
-  gyroZ = imu.calcGyro(imu.gz);
-    
-  Serial.println("Accel: " + String(accelX) + ", " +
-              String(accelY) + ", " + String(accelZ) + " g");
-  Serial.println("Gyro: " + String(gyroX) + ", " +
-              String(gyroY) + ", " + String(gyroZ) + " dps");
-  Serial.println("Time: " + String(imu.time) + " ms");
-  Serial.println();
-  delay(1000);
+  Serial.println("Angle: " + String(acc_angle_y));
 }
+re_command check_receiver()
+{
+  int re_1_state = digitalRead(re_1);
+  int re_2_state = digitalRead(re_2);
+  int re_3_state = digitalRead(re_3);
+  int re_4_state = digitalRead(re_4);
+  re_command command = stay;
+  if (re_4_state == 1) {  // forward
+    
+  }
+  else if (re_3_state == 1) {  // backward
+    
+  }
+  else if (re_2_state == 1) {  // left
+    
+  }
+  else if (re_1_state == 1) {  // right
+    
+  }
+  else {                       // Do nothing and keep balance
+    
+  }
+
+  return re_command;
+}
+
 
 
 /*======================setup======================*/
@@ -64,20 +82,13 @@ void setup() {
   // MPU-9250
   if (imu.begin() != INV_SUCCESS)
   {
-    while (1)
-    {
-      Serial.println("Unable to communicate with MPU-9250");
-      Serial.println("Check connections, and try again.");
-      Serial.println();
-      delay(5000);
-    }
+    imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);// Enable all sensors:
+    imu.setGyroFSR(2000); // Set gyro to 2000 dps
+    imu.setAccelFSR(2); // Set accel to +/-2g
+    imu.setLPF(5); // Set LPF corner frequency to 5Hz
+    imu.setSampleRate(10); // Set sample rate to 10Hz
+    imu.setCompassSampleRate(10); // Set mag rate to 10Hz
   }
-  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);// Enable all sensors:
-  imu.setGyroFSR(2000); // Set gyro to 2000 dps
-  imu.setAccelFSR(2); // Set accel to +/-2g
-  imu.setLPF(5); // Set LPF corner frequency to 5Hz
-  imu.setSampleRate(10); // Set sample rate to 10Hz
-  imu.setCompassSampleRate(10); // Set mag rate to 10Hz
   
   // L298N
   pinMode(dir1PinA, OUTPUT);
@@ -86,37 +97,45 @@ void setup() {
   pinMode(dir1PinB, OUTPUT);
   pinMode(dir2PinB, OUTPUT);
   pinMode(speedPinB, OUTPUT);
+
+  // Receiver
+  pinMode(re_1, INPUT);
+  pinMode(re_2, INPUT);
+  pinMode(re_3, INPUT);
+  pinMode(re_4, INPUT);
 }
 
 /*======================main loop======================*/
 void loop() {
 
+  // Update IMU data
   if ( imu.dataReady() )
   {
     imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
     UpdateIMUData();
+    printIMUData();
   }
 
-  // Initialize the Serial interface:
-
+  // Check the receive message  
+  re_command command = check_receiver();
+  if (command == forward) {
+  }
+  else if (command == backward) {
+  }
+  else if (command == left) {
+  }
+  else if (command == right) {
+  }
+  else {
+  }
+/* keyboard test
   if (Serial.available() > 0) {
     int input_command = Serial.read();
-
     switch (input_command) {
-      //______________Motor 1______________
-
       case '1': // Motor 1 Forward
         analogWrite(speedPinA, 255);//Sets speed variable via PWM
         digitalWrite(dir1PinA, LOW);
         digitalWrite(dir2PinA, HIGH);
-        Serial.println("Motor 1  Forward"); // Prints out “Motor 1 Forward” on the serial monitor
-        Serial.println("   "); // Creates a blank line printed on the serial monitor
-
-        analogWrite(speedPinB, 255);
-        digitalWrite(dir1PinB, LOW);
-        digitalWrite(dir2PinB, HIGH);
-        Serial.println("Motor 2 Forward");
-        Serial.println("   ");
         break;
 
       case '2': // Motor 1 Stop (Freespin)
@@ -160,6 +179,7 @@ void loop() {
         Serial.println("Motor 2 Reverse");
         Serial.println("   ");
         break;
+*/
     }
   }
 }
